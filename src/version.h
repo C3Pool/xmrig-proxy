c6c11f6714bb2de32f4032643a9c75c8d5576b8a
C3Pool <hkfoan@gmail.com>
2020-12-16 16:26:48 +0800

    Revert "Merge xmrig-cuda v6.5.0 into master"
    This reverts commit 8486651753e6876a1cc53c038c4dcce9219dee78.

--- src/version.h
+++ src/version.h
@@ -25,18 +25,35 @@
 #ifndef XMRIG_VERSION_H
 #define XMRIG_VERSION_H
 
-#define APP_ID        "xmrig-cuda"
-#define APP_NAME      "XMRig"
-#define APP_DESC      "XMRig CUDA plugin"
-#define APP_VERSION   "6.5.0-C3"
+#define APP_ID        "xmrig-proxy"
+#define APP_NAME      "xmrig-proxy"
+#define APP_DESC      "XMRig Stratum proxy"
+#define APP_VERSION   "6.3.0-C3"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2020 xmrig.com"
+#define APP_KIND      "proxy"
 
 #define APP_VER_MAJOR  6
-#define APP_VER_MINOR  5
+#define APP_VER_MINOR  3
 #define APP_VER_PATCH  0
 
-#define API_VERSION    3
+#ifdef _MSC_VER
+#   if (_MSC_VER >= 1920)
+#       define MSVC_VERSION 2019
+#   elif (_MSC_VER >= 1910 && _MSC_VER < 1920)
+#       define MSVC_VERSION 2017
+#   elif _MSC_VER == 1900
+#       define MSVC_VERSION 2015
+#   elif _MSC_VER == 1800
+#       define MSVC_VERSION 2013
+#   elif _MSC_VER == 1700
+#       define MSVC_VERSION 2012
+#   elif _MSC_VER == 1600
+#       define MSVC_VERSION 2010
+#   else
+#       define MSVC_VERSION 0
+#   endif
+#endif
 
 #endif /* XMRIG_VERSION_H */
